e7071856196926a94c2d84bd32d85ac5626c31a4
Jen Miao <jenmiao@gmail.com>
2020-02-25 06:38:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -18,8 +18,9 @@ class Shoe
   def cobble
     #new = Shoe.condition
     #new = Shoe.condtion("new")
-    Shoe.condtion = "new"
-    Shoe.condition 
+    Shoe.condtion=("new")
+    # Shoe.condition
+
     puts "Your shoe is as good as new!"
 
   end
